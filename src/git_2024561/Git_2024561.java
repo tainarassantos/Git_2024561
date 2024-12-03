fbe8e8459dcb9d40d41e24d5d8b2de0b618d8777
tainarassantos <souza.tainara127@gmail.com>
2024-12-03 19:32:15 +0000

    Introduction in Programing, CCT College Homework - Simple Git Test

---
+++ src/git_2024561/Git_2024561.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package git_2024561;
+
+/**
+ *
+ * @author Tainara de Souza Santos
+ * 
+ * Homework Submission - 3%
+The purpose of this Homework is to make sure that you can set up and use git & github with Netbeans.
+
+TASK AS FOLLOWS
+1) Set up a NEW PROJECT using Git_xxxxxxx as the project name, where xxxxxxx is your Student ID number (e.g. Git_2024999)
+2) Initialise the Git repository and commit the (empty!) project.
+3) Publish it to github. It is probably easiest to do this using git desktop.
+IMPORTANT -- Make sure that you make this public! There is a 'tick box' that sets the project as private, so make sure this is NOT TICKED!
+4) Add a line of code to your project with a simple output. This should print your name and student ID. (e.g. System.out.println(" Ken - 2024999")
+5) Commit the change with a simple commit message (e.g. 'added a simple print') and use git desktop to push the change to github. Note that this should mean that you have 2 commits on your project.
+6) Upload the github URL for this project here. You just need to upload a .txt file with the URL
+LAST CHECK -- Go to the repository ("repo") on github. After the name, it should say 'public'. If it is private then you MUST change this! Click Settings. Scroll to the bottom ('Danger Zone') and select 'Change visibility' and you will be able to 'Change to Public'.
+DO NOT UPLOAD ANY CODE TO MOODLE! YOUR TASK IS TO HAVE THE PROJECT ON GITHUB!
+ONLY .txt FILES WILL BE ACCEPTED HERE!
+ * 
+ */
+public class Git_2024561 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
